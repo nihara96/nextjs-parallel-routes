41dc657bea9d22956d5bf26e466d6c2d4069a497
nihara <nbmayurawasala@gmail.com>
2023-08-01 10:27:47 +0530

    moved to homepage

---
+++ src/app/@left/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>Loading Left...</div>
+  )
+}
+
+export default Loading;
---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>Loading Main</div>
+  )
+}
+
+export default Loading; 

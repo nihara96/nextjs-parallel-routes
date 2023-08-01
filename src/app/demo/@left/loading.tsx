76bbfdc13c384d1c7bd228cba5a7afcbab3cbedf
nihara <nbmayurawasala@gmail.com>
2023-08-01 10:30:03 +0530

    moved to homepage

--- src/app/demo/@left/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Loading = () => {
-  return (
-    <div>Loading Left...</div>
-  )
-}
-
-export default Loading;
--- src/app/demo/@right/loading.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Loading = () => {
-  return <div>Loading Right...</div>;
-};
-
-export default Loading;
